c3995c031c89520ca2f74d4ef33f3b97d30e7a78
Supanuth Ongsuk <n.nsupanuth@gmail.com>
2020-06-20 16:57:53 +0700

    Initialize template for spring security with jwt

---
+++ src/main/java/com/example/springsecurityjwt/models/AuthenticationResponse.java
@@ -0,0 +1,14 @@
+package com.example.springsecurityjwt.models;
+
+import lombok.Data;
+
+@Data
+public class AuthenticationResponse {
+
+    private final String jwt;
+
+    public AuthenticationResponse(String jwt) {
+        this.jwt = jwt;
+    }
+
+}
